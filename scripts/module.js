b5302900546e0b77efa3bb07a4b4acdcb30c68e1
Guin Kellam <guin.kellam@vidyard.com>
2021-03-13 18:35:41 -0500

    test

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,12 @@
 Hooks.once('init', async function() {
-
+  console.log('init');
+  window.addEventListener("message", (event) => {
+    console.log(event);
+    console.log("message recieved");
+  }, false);
 });
 
 Hooks.once('ready', async function() {
+  console.log('ready');
 
 });
