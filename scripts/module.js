a86961461a20d064643bc9e9bdd154a1d9fb6180
Guin Kellam <guin.kellam@vidyard.com>
2021-03-20 14:10:25 -0400

    accept roll

--- scripts/module.js
+++ scripts/module.js
@@ -5,9 +5,13 @@ Hooks.once('init', async function() {
     console.log("message recieved");
   }, false);
 
-  window.addEventListener("roll", (event) => {
+  window.addEventListener("roll", (event, type, options) => {
     console.log(event);
+    console.log(type);
+    console.log(options);
+
     console.log("roll recieved");
+    view1.getInt32(0).toString(16)
   }, false);
 });
 
