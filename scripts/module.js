70a707213c2430f1ebe83c34144cf6e19cddfe8c
Guin Kellam <guin.kellam@vidyard.com>
2021-03-20 14:49:42 -0400

    roll work

--- scripts/module.js
+++ scripts/module.js
@@ -3,14 +3,17 @@ Hooks.once('init', async function() {
   window.addEventListener("message", (event) => {
     console.log(event);
     console.log("message recieved");
-  }, false);
-
-  window.addEventListener("roll", (event, type, options) => {
-    console.log(event);
-    console.log("roll recieved");
     let roll = new Roll(event.data.data);
     roll.evaluate();
     roll.toMessage();
+  }, false);
+
+  // window.addEventListener("roll", (event, type, options) => {
+  //   console.log(event);
+  //   console.log("roll recieved");
+  //   let roll = new Roll(event.data.data);
+  //   roll.evaluate();
+  //   roll.toMessage();
   }, false);
 });
 
